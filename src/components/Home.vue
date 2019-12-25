4c234febc3ec4c4be8f17394081ac667075d8629
田怡文 <tianyiwen@zallds.com>
2019-12-25 12:29:16 +0800

    add files

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,45 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/home-logo2.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>导航一</span>
+            </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧主体区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
 <script>
 export default {
   methods: {
@@ -14,5 +51,37 @@ export default {
   }
 }
 </script>
+
 <style lang="scss" scoped>
+.home-container {
+  height: 100%;
+}
+// 头部样式
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    img {
+      width: 34px;
+      height: 34px;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+// 侧边栏样式
+.el-aside {
+  background-color: #333744;
+}
+// 主体样式
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
