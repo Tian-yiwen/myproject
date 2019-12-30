2fdbfafe38f34057737b3d962d45b8186bff3157
Tian-yiwen <571181523@qq.com>
2019-12-30 22:46:40 +0800

    角色列表渲染

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -8,8 +8,8 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
     <el-card>
-      <el-table :data="rightsList">
-        <el-table-column type="index"></el-table-column>
+      <el-table :data="rightsList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
         <el-table-column label="权限名称" prop="authName"></el-table-column>
         <el-table-column label="路径" prop="path"></el-table-column>
         <el-table-column label="权限等级" prop="level">
