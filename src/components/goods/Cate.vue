f76d2a6d2f28435e9326e2fd1347b20f0b659189
tianyiwen <tianyiwen@zallds.com>
2020-01-03 17:51:57 +0800

    下拉菜单的高度还没有解决

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,7 +1,240 @@
 <template>
-  <div>商品分类</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 添加分类按钮区域 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary" @click="showAddCateDialog"
+            >添加分类</el-button
+          >
+        </el-col>
+      </el-row>
+      <!-- 分类列表区域 -->
+      <tree-table
+        class="treeTable"
+        :data="catelist"
+        :columns="columns"
+        :selection-type="false"
+        :expand-type="false"
+        show-index
+        index-text="#"
+        border
+        :show-row-hover="false"
+      >
+        <!-- 是否有效插槽 -->
+        <template slot="isok" slot-scope="scope">
+          <i
+            class="el-icon-success"
+            v-if="scope.row.cat_deleted === false"
+            style="color:lightgreen;"
+          ></i>
+          <i class="el-icon-error" v-else style="color:red;"></i>
+        </template>
+        <!-- 排序插槽 -->
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level === 0">一级</el-tag>
+          <el-tag
+            type="success"
+            size="mini"
+            v-else-if="scope.row.cat_level === 1"
+            >二级</el-tag
+          >
+          <el-tag type="warning" size="mini" v-else>三级</el-tag>
+        </template>
+        <!-- 操作插槽 -->
+        <template slot="option">
+          <el-button type="primary" icon="el-icon-edit" size="mini"
+            >编辑</el-button
+          >
+          <el-button type="danger" icon="el-icon-delete" size="mini"
+            >删除</el-button
+          >
+        </template>
+      </tree-table>
+      <!-- 分页效果 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :page-size="queryInfo.pagesize"
+        layout="total, prev, pager, next, jumper"
+        :total="total"
+        :current-page="queryInfo.pagenum"
+      >
+      </el-pagination>
+    </el-card>
+    <!-- 添加分类对话框 -->
+    <el-dialog
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      @close="addCateDialogClosed"
+    >
+      <!-- 内容区域-添加表单 -->
+      <el-form
+        :model="addCateForm"
+        :rules="addCateFormRules"
+        ref="addCateFormRef"
+        label-width="80px"
+      >
+        <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级分类">
+          <el-cascader
+            v-model="selectedKeys"
+            expandTrigger="hover"
+            :options="parentCateList"
+            :props="cascaderProps"
+            @change="parentCateChanged"
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+      <!-- 底部区域 -->
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 查询条件
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类的数据列表，默认为空
+      catelist: [],
+      // 总数据条数
+      total: 0,
+      // 为table指定列的定义
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 表示将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'isok'
+        },
+        {
+          label: '排序',
+          // 表示将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'order'
+        },
+        {
+          label: '操作',
+          // 表示将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用模板名称
+          template: 'option'
+        }
+      ],
+      // 添加分类的data
+      addCateDialogVisible: false,
+      addCateForm: {
+        cat_name: '',
+        cat_pid: 0,
+        // 分类等级默认要添加的是一级分类
+        cat_level: 0
+      },
+      parentCateList: [],
+      addCateFormRules: {
+        cat_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '分类名称在3~10个字符之间',
+            trigger: 'blur'
+          }
+        ]
+      },
+      // 选中的父级分类的id数
+      selectedKeys: [],
+      cascaderProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      }
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    // 获取商品分类数据
+    async getCateList() {
+      console.log('编辑删除的插槽还需要scope')
+      const { data: res } = await this.$http.get('categories', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品数据失败！')
+      } else {
+        this.catelist = res.data.result
+        this.total = res.data.total
+      }
+    },
+
+    // 监听pagesize改变的事件
+    handleSizeChange(newSize) {
+      // console.log(newSize)
+      this.queryInfo.pagesize = newSize
+      this.getCateList()
+    },
+    // 监听页码值改变的事件
+    handleCurrentChange(newPage) {
+      // console.log(newPage)
+      this.queryInfo.pagenum = newPage
+      this.getCateList()
+    },
+    // 添加分类的方法函数
+    showAddCateDialog() {
+      // 先获取父级列表数据在展示对话框
+      this.getParentCateList()
+      this.addCateDialogVisible = true
+    },
+    addCateDialogClosed() {
+      this.$refs.addCateFormRef.resetFields()
+    },
+    async getParentCateList() {
+      // 获取父级分类的数据列表
+      const { data: res } = await this.$http.get('categories', {
+        params: { type: 2 }
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取父级列表数据失败')
+      } else {
+        this.parentCateList = res.data
+      }
+    },
+    // 选择项发生变化时触发的事件
+    parentCateChanged() {
+      console.log(this.selectedKeys)
+    },
+    addCate() {}
+  }
+}
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.treeTable {
+  margin-top: 15px;
+}
+</style>
