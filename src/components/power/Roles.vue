dd9ed081263471667c1e7652b130d0a3c980abbb
tianyiwen <tianyiwen@zallds.com>
2020-01-03 09:47:31 +0800

    完成用户管理和权限管理

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -11,7 +11,9 @@
       <!-- 添加角色按钮区域 -->
       <el-row>
         <el-col>
-          <el-button type="primary">添加角色</el-button>
+          <el-button type="primary" @click="addDialogRoles = true"
+            >添加角色</el-button
+          >
         </el-col>
       </el-row>
       <!-- 角色列表区域 -->
@@ -19,17 +21,54 @@
         <!-- 展开列 -->
         <el-table-column type="expand">
           <template slot-scope="scope">
-            <el-row :class="['bdbottom',i1===0?'bdtop':'']" v-for="(item1,i1) in scope.row.children" :key="item1.id">
+            <el-row
+              :class="['bdbottom', i1 === 0 ? 'bdtop' : '']"
+              v-for="(item1, i1) in scope.row.children"
+              :key="item1.id"
+            >
               <!-- 渲染一级权限 -->
               <el-col :span="5">
-                <el-tag>
-                  {{ item1.authName }}
-                </el-tag>
+                <el-tag
+                  closable
+                  @close="removeRightById(scope.row, item1.id)"
+                  >{{ item1.authName }}</el-tag
+                >
                 <i class="el-icon-caret-right"></i>
               </el-col>
               <!-- 渲染二级和三级权限 -->
-              <el-col :span="19"></el-col>
+              <el-col :span="19">
+                <!-- 通过 for 循环 嵌套渲染二级权限 -->
+                <el-row
+                  :class="[i2 === 0 ? '' : 'bdtop', 'vcenter']"
+                  v-for="(item2, i2) in item1.children"
+                  :key="item2.id"
+                >
+                  <el-col :span="6">
+                    <el-tag
+                      type="success"
+                      closable
+                      @close="removeRightById(scope.row, item2.id)"
+                      >{{ item2.authName }}</el-tag
+                    >
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag
+                      type="warning"
+                      v-for="(item3) in item2.children"
+                      :key="item3.id"
+                      closable
+                      @close="removeRightById(scope.row, item3.id)"
+                      >{{ item3.authName }}</el-tag
+                    >
+                  </el-col>
+                </el-row>
+              </el-col>
             </el-row>
+
+            <!-- <pre>
+              {{scope.row}}
+            </pre> -->
           </template>
         </el-table-column>
         <!-- 索引列 -->
@@ -43,12 +82,21 @@
               v-if="scope.row.children"
               type="primary"
               icon="el-icon-edit"
+              @click="showEditDialogRoles(scope.row.id)"
               >编辑</el-button
             >
-            <el-button size="mini" type="danger" icon="el-icon-delete"
+            <el-button
+              size="mini"
+              type="danger"
+              icon="el-icon-delete"
+              @click="removeRoleById(scope.row.id)"
               >删除</el-button
             >
-            <el-button size="mini" type="warning" icon="el-icon-setting"
+            <el-button
+              size="mini"
+              type="warning"
+              icon="el-icon-setting"
+              @click="showSetRightDialog(scope.row)"
               >分配权限</el-button
             >
           </template>
@@ -236,6 +284,7 @@ export default {
     // 展示修改角色对话框的信息
     async showEditDialogRoles(id) {
       const { data: res } = await this.$http.get('roles/' + id)
+
       if (res.meta.status !== 200) {
         return this.$message.error('查询角色信息失败！')
       } else {
@@ -378,13 +427,17 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.el-tag{
-    margin: 7px;
+.el-tag {
+  margin: 7px;
 }
-.bdtop{
-border-top: 1px solid #eee;
+.bdtop {
+  border-top: 1px solid #eee;
 }
-.bdbottom{
+.bdbottom {
   border-bottom: 1px solid #eee;
+}
+.vcenter {
+  display: flex;
+  align-items: center;
 }
 </style>
