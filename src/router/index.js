2d98c8674c2ec1db6952f8f5c97e06884b9a1599
tianyiwen <tianyiwen@zallds.com>
2020-01-03 10:50:56 +0800

    完成了用户和权限

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
+
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
