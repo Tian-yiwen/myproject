8acf88211dc58b0a8c30fa3996ddb55aae9ddaee
tianyiwen <tianyiwen@zallds.com>
2020-01-09 14:33:41 +0800

    11

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -10,8 +10,18 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <!-- 查询 -->
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getGoodsList"
+          >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="getGoodsList"
+            ></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
@@ -19,29 +29,92 @@
         </el-col>
       </el-row>
       <!-- 商品列表区域 -->
-      <el-table border stripe>
+      <el-table border stripe :data="goodslist">
         <el-table-column label="#" type="index"></el-table-column>
-        <el-table-column label="商品名称"></el-table-column>
-        <el-table-column label="商品价格（元）"></el-table-column>
-        <el-table-column label="商品重量"></el-table-column>
-        <el-table-column label="创建时间"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
-        <!-- 修改删除操作的插槽 -->
-        <template>
-          <!-- 修改 -->
-          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-          <!-- 删除 -->
-          <el-button
-            type="danger"
-            icon="el-icon-delete"
-            size="mini"
-          ></el-button>
-        </template>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column
+          label="商品价格（元）"
+          prop="goods_price"
+          width="95px"
+        ></el-table-column>
+        <el-table-column
+          label="商品重量"
+          prop="goods_weight"
+          width="70px"
+        ></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="140px">
+          <template slot-scope="scope">
+            {{ scope.row.add_time | dataFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <!-- 修改删除操作的插槽 -->
+          <template>
+            <!-- 修改 -->
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <!-- 删除 -->
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+            ></el-button>
+          </template>
+        </el-table-column>
       </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 4, 8]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 获取商品列表的参数
+      queryInfo: {
+        query: '',
+        // 当前的页数
+        pagenum: 1,
+        // 当前每页显示多少条数据
+        pagesize: 8
+      },
+      goodslist: [],
+      total: 0
+    }
+  },
+  created() {
+    this.getGoodsList()
+  },
+  methods: {
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品信息失败')
+      } else {
+        this.goodslist = res.data.goods
+        this.total = res.data.total
+        this.queryInfo.pagenum = res.data.pagenum
+      }
+    },
+    handleSizeChange() {},
+    handleCurrentChange() {}
+  }
+}
 </script>
 <style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -24,7 +24,16 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
-
+Vue.filter('dataFormat', function(originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + 1 + '').padStart(2, '0')
+  const hh = (dt.getHours() + 1 + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + 1 + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + 1 + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   render: h => h(App)
